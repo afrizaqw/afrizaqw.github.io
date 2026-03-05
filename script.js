f952135f409ff0f1f50d0a47be0a557055a2cd33
afrizaqw <afrizalow@gmail.com>
2026-03-06 02:06:39 +0700

    look update

--- script.js
+++ script.js
@@ -2,21 +2,25 @@
 let slideIndex = 0;
 
 function showSlides() {
-    let slides = document.getElementsByClassName("mySlides");
-    
-    // Sembunyiin semua slide
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    
-    // Tampilkan slide yang aktif
-    slides[slideIndex-1].style.display = "block";  
-    
-    // Ganti slide tiap 4 detik
-    setTimeout(showSlides, 4000); 
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active-dot", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active-dot";
+  
+  setTimeout(showSlides, 4000); 
 }
 
 // Jalankan slider saat halaman dibuka
